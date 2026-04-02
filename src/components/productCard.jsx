ed424408fa0439f605936104f349e6e5c536040f
methuliahangama <methuliahangama13@gmail.com>
2026-04-02 17:43:33 +0530

    search products

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function ProductCard(props) {
 
     console.log(props);
-    
+
     return (
-       <Link to={`/productInfo/${props.product.productID}`} >
-        <div className="w-[300px] h-[400px] m-[50px] rounded-xl shadow-lg shadow-gray-500 hover:shadow-primary hover:border-gray-500 hover:border-[3px] transition-shadow duration-300 flex flex-col items-center gap-4 p-4 justify-center">
+        <Link to={`/productInfo/${props.product.productID}`} className="w-[300px] h-[400px] m-[50px] rounded-xl shadow-lg shadow-gray-500 hover:shadow-primary hover:border-gray-500 hover:border-[3px] transition-shadow duration-300 flex flex-col items-center gap-4 p-4 justify-center" >
+
             <img src={props.product.images[0]} alt="Product Image" className="w-full h-[65%] object-cover rounded-md overflow-hidden flex flex-col" />
             <div className="flex flex-col gap-2">
                 <h2 className="text-3xl font-semibold text-center">{props.product.productName}</h2>
@@ -18,9 +18,9 @@ export default function ProductCard(props) {
                 }
                 <p className="font-semibold text-left text-lg text-gray-600 line-through">LKR.{props.product.lastPrice.toFixed(2)}</p>
                 <p className="text-gray-600">{props.product.description}</p>
-                
+
             </div>
-        </div>
-       </Link>
+
+        </Link>
     )
 }
--- src/pages/home/product.jsx
+++ src/pages/home/product.jsx
@@ -6,15 +6,17 @@ import ProductCard from "../../components/productCard";
 export default function ProductPage() {
     const [products, setProducts] = useState([]);
     const [loadingStatus, setLoadingStatus] = useState("loading");
+    const [query, setQuery] = useState("");
     useEffect(
         () => {
             if (loadingStatus === "loading") {
                 axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products")
                     .then(
                         (res) => {
-                            console.log(res.data);
+                            //console.log(res.data);
                             setProducts(res.data);
                             setLoadingStatus("loaded");
+                            setProductsLoaded(true);
                         }
                     ).catch(
                         (err) =>
@@ -23,16 +25,64 @@ export default function ProductPage() {
             }
 
         }, []);
-    return (
-        <div className="w-full h-full overflow-y-scroll flex flex-wrap justify-center">
-            {
-                products.map(
-                    (product) =>
-                        <ProductCard product={product} />
 
+    function search(e) {
+        const query = e.target.value;
+        setQuery(query);
+        setLoadingStatus("loading");
+        if (query == "") {
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products")
+                .then(
+                    (res) => {
+                        //console.log(res.data);
+                        setProducts(res.data);
+                        setLoadingStatus("loaded");
+                    }
+                ).catch(
+                    (err) =>
+                        toast.error("Failed to load products")
                 )
+        } else {
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products/search/" + query)
+                .then(
+                    (res) => {
+                        //console.log(res.data);
+                        setProducts(res.data);
+                        setLoadingStatus("loaded");
+                    }
+                ).catch(
+                    (err) =>
+                        toast.error("Failed to load products")
+                );
+        }
+    }
 
-            }
+
+    return (
+        <div className="w-full h-full pt-4 relative">
+            <div className="absolute w-full flex justify-center">
+                <input type="text"
+                    placeholder="Search products..."
+                    className="w-1/2 p-2 absolute z-50"
+                    onChange={search}
+                    value={query}
+                />
+            </div>
+            {loadingStatus === "loaded" && <div className="w-full h-full overflow-y-scroll flex flex-wrap justify-center pt-4 relative">
+
+                {
+                    products.map(
+                        (product) =>
+                            <ProductCard product={product} />
+
+                    )
+
+                }
+            </div>}
+            {loadingStatus === "loading" && <div className="w-full h-full flex items-center justify-center">
+                <div className="animate-spin rounded-full h-32 w-32 border-2 border-gray-800 border-b-accent b-4">
+                </div>
+            </div>}
         </div>
     );
 }
